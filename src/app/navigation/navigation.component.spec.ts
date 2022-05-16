fc7ebc44b8ad9c632c926ae6d9aa4d949335acfd
Dennis Cai <dennis@thecodinganalyst.com>
2022-05-16 11:46:49 +0800

    add material ui, navigation and testing-library

---
+++ src/app/navigation/navigation.component.spec.ts
@@ -0,0 +1,51 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { NavigationComponent } from './navigation.component';
+import {render, screen} from "@testing-library/angular";
+
+describe('NavigationComponent', () => {
+  let component: NavigationComponent;
+  let fixture: ComponentFixture<NavigationComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [NavigationComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
+
+describe('NavigationComponent', () => {
+  it('should render header according to title', async () => {
+    await render(NavigationComponent, {
+      componentProperties: { title: 'hello'}
+    })
+    expect(screen.getByRole('banner').innerHTML).toBe('hello');
+  })
+});
