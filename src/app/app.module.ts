c5df9673a33625155420d0639875c9b8f6a5e43f
Dennis Cai <dennis@thecodinganalyst.com>
2022-05-16 14:40:18 +0800

    add chart of accounts components and navigation link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ChartOfAccountsComponent } from './chart-of-accounts/chart-of-accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ChartOfAccountsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
@@ -18,6 +19,9 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
 
 }
